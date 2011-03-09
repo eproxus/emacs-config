fd79a1d5f976eda6223647a784d1be25b12a27bb
Adam Lindberg <eproxus@gmail.com>
2011-03-09 19:30:44 +0100

    Change default font size to 12 pt (from 14 pt)

--- emacs.el
+++ emacs.el
@@ -112,7 +112,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#2e3436" :foreground "#d3d7cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Menlo"))))
+ '(default ((t (:inherit nil :stipple nil :background "#2e3436" :foreground "#d3d7cf" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :family "Menlo"))))
  '(compilation-error ((t (:inherit font-lock-warning-face :foreground "red"))))
  '(cursor ((t (:background "white" :foreground "black"))))
  '(flymake-errline ((((class color) (background dark)) (:background "#691A1B" :underline "#ef2929" :weight bold))))
